0bf5202c92a2fb41547c79a9208e9d32ac59ae57
canan-ozedemir-kidsloop <101562135+canan-ozedemir-kidsloop@users.noreply.github.com>
2024-03-19 12:25:01 +0000

    first commit

--- e2e/firstTest.spec.ts
+++ e2e/firstTest.spec.ts
@@ -1,7 +1,46 @@
 import {test,expect} from  '@playwright/test'
 
 
-test("open the page",async ({page}) => {
-    await  page.goto('https://fakestoreapi.com/docs');
-    await expect(page).toHaveURL('https://fakestoreapi.com/docs');
+// test("open the page",async ({page}) => {
+//     await  page.goto('https://fakestoreapi.com/docs');
+//     await expect(page).toHaveURL('https://fakestoreapi.com/docs');
+// })
+
+test('GET REQUEST',async ({request}) => {
+    const response= await request.get("https://fakestoreapi.com/products/1")
+
+    console.log(await response.json());
+    expect(response.status()).toBe(200);
+    const value=await response.json();
+    console.log(value.id);
+    expect(value.id).toBe(1);
+
+    expect(value).toHaveProperty('title', 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops');
+
+
+})
+
+
+test('POST REQUEST',async ({request}) => {
+    const response= await request.post("https://fakestoreapi.com/products",{
+
+   data:{
+    title: 'test product',
+    price: 13.5,
+    description: 'lorem ipsum set',
+    image: 'https://i.pravatar.cc',
+    category: 'electronic'
+
+
+   }
+
+
+
+    })
+
+    expect(response.status()).toBe(200);
+
+
+
+
 })
